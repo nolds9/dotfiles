46ffd560441827b4e74392bf43459cc33757dcb1
Nicholas Olds <nolds@costar.com>
2019-07-22 11:27:04 -0400

    Add CoStar dotfiles

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -1,216 +1,48 @@
-############################################
-#  Navigation - Movement & Manipulation
-############################################
+# Detect which `ls` flavor is in use
+if ls --color > /dev/null 2>&1; then # GNU `ls`
+  colorflag="--color"
+else # OS X `ls`
+  colorflag="-G"
+fi
 
-alias ..="cd .."
-alias ...="cd ../.."
-alias ....="cd ../../.."
-alias -- -="cd -"
+# List all files colorized in long format
+alias ll='ls -lh'
 
-# cd into whatever is the forefront Finder window.
-function cdf() {  # short for cdfinder
-  cd "`osascript -e 'tell app "Finder" to POSIX path of (insertion location as alias)'`"
-}
+# List all files colorized in long format, including dot files
+alias la="ls -lha"
 
-# Create a new directory and enter it
-function md() {
-	mkdir -p "$@" && cd "$@"
-}
+# List only directories
+alias lsd='ls -l | grep "^d"'
 
-alias mv='mv -v'
-alias rm='rm -i -v'
-alias cp='cp -v'
+# Always use color output for `ls`
+alias ls="command ls ${colorflag}"
+export LS_COLORS='no=00:fi=00:di=01;34:ln=01;36:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arj=01;31:*.taz=01;31:*.lzh=01;31:*.zip=01;31:*.z=01;31:*.Z=01;31:*.gz=01;31:*.bz2=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.jpg=01;35:*.jpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.avi=01;35:*.fli=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.ogg=01;35:*.mp3=01;35:*.wav=01;35:'
 
-alias note="open -a Typora"
-alias hst="open -a Hearthstone"
-############################################
-#  Navigation - Search & Display
-############################################
+alias ..="cd ../"
+alias ...="cd ../../"
 
-# `cat` with beautiful colors. requires: sudo easy_install -U Pygments
-alias c='pygmentize -O style=monokai -f console256 -g'
-
-# brew install the_silver_searcher
-alias ag='ag -f --hidden'
-
-alias tre="tree -CshDL 1" # -L one level deep
-alias tre2="tree -CshDL 2" # -L two levels deep
-alias tre3="tree -CshDL 3" # -L two levels deep
-alias tred="tree -CshDd" # -d directories only
-alias trea="tree -CshDaL 1" # -L one level deep, including '.' files
-alias trea2="tree -CshDaL 2" # -L two levels deep, including '.' files
-alias trea3="tree -CshDaL 3" # -L two levels deep, including '.' files
-alias treaa="tree -CshDa" # -a all including '.' files
-alias tree="tree -CshD" # Color, size, date
-alias treee="tree"
-
-# find shorthand
-function f() {
-	find . -name "$1" 2>&1 | grep -v 'Permission denied'
-}
-
-# use coreutils `ls` if possible…
-hash gls >/dev/null 2>&1 || alias gls="ls"
-
-# always use color, even when piping (to awk,grep,etc)
-if gls --color > /dev/null 2>&1; then colorflag="--color"; else colorflag="-G"; fi;
-export CLICOLOR_FORCE=1
-
-# ls options: A = include hidden (but not . or ..), F = put `/` after folders, h = byte unit suffixes
-alias ls='gls -AFh ${colorflag} --group-directories-first'
-alias lsd='ls -l | grep "^d"' # only directories
-alias lsl="ls -lhF" # long format, human readable, classify
-alias lss="ls -GhF" # no group, human readable, classify
-
-# List all files, long format, colorized, permissions in octal
-function la(){
- 	ls -l  "$@" | awk '
-    {
-      k=0;
-      for (i=0;i<=8;i++)
-        k+=((substr($1,i+2,1)~/[rwx]/) *2^(8-i));
-      if (k)
-        printf("%0o ",k);
-      printf(" %9s  %3s %2s %5s  %6s  %s %s %s\n", $3, $6, $7, $8, $5, $9,$10, $11);
-    }'
-}
-
-##############################
-#  Git Aliases
-##############################
-
-alias g="git"
-
-alias gba="git branch -a"
-alias gcb="git checkout -b"
-
-alias ga="git add"
+# Git Aliases
+alias gco="git checkout"
+alias gcom="git checkout master"
 alias gs="git status"
+alias gcb="git checkout -b"
 alias gcm="git commit -m"
-alias gpom="git push origin master"
-alias go="git-open"
+alias gpl="git pull"
+alias gps="git push"
+alias gll="git log --oneline --decorate --all --graph --stat"
+alias gdf="git diff"
+alias gunpsh="git log --branches --not --remotes --simplify-by-decoration --decorate --oneline --stat"
+alias gahd="git diff origin/master..HEAD"
+alias glg="git log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
+alias guncm="git reset HEAD^"
+alias grb="git branch --sort=-committerdate"
 
-alias gl="git log --all --oneline --graph --decorate"
-alias gla='git log --all --decorate --graph --pretty=format:"%C(yellow)%h%Creset %C(auto)%d%Creset %Cblue%ar%Creset %Cred%an%Creset %n%w(72,1,2)%s"'
-alias gpr="git log --pretty=format:'%Cblue%h%Creset %Cgreen%ad%Creset | %s%C(yellow)%d%Creset [%an]' --graph --date=short --decorate"
-
-# github-email <user> https://github.com/paulirish/github-email
-# diffshot https://github.com/RobertAKARobin/diffshot
-
-# git clone & then cd
-function gc {
-  reponame=${1##*/}
-  reponame=${reponame%.git}
-  git clone "$1" "$reponame";
-  cd "$reponame";
-}
-
-function gitsearch(){
-  git rev-list --all | xargs git grep -i $1
-}
-
-function gh(){
-  giturl=$(git config --get remote.origin.url)
-  if [ "$giturl" == "" ]
-    then
-     echo "Not a git repository or no remote.origin.url set"
-     exit 1;
-  fi
-
-  giturl=${giturl/git\@github\.com\:/https://github.com/}
-  giturl=${giturl/\.git}
-  open $giturl
-}
-
-##############################
-# Firebase
-##############################
-
-alias fs="firebase serve"
-alias fu="firebase use"
-alias fo="firebase open"
-alias fd="firebase deploy"
-alias fl="firebase login"
-alias fin="firebase init"
-alias fup="npm i -g firebase-tools"
-
-##############################
-#  Other Aliases
-##############################
-
-alias obash="cd ~/dev/dotfiles; atom ."
-alias rbash="source ~/.bash_profile"
-
-alias rspecc="rspec -c"
-alias rspecf="rspec -c -fd"
-
-alias chro="open -a 'Google Chrome'"
-alias a="atom"
-
-# alias oops='$(thefuck $(fc -ln -1))'
-
-# alias w='ansiweather'
-
-##############################
-#  Network Aliases
-##############################
-
-alias jn="jupyter notebook"
-# Networking. IP address, dig, DNS
-alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
-alias dig="dig +nocmd any +multiline +noall +answer"
-# wget sucks with certificates. Let's keep it simple.
-alias wget="curl -O"
-
-alias hs-o="http-server -o"
-
-# Start an HTTP server from a directory, optionally specifying the port
-function server() {
-	local port="${1:-8000}"
-	open "http://localhost:${port}/" &
-	# statik is good because it won't expose hidden folders/files by default.
-	# npm install -g statik
-	statik --port "$port" .
-}
-
-# get gzipped size
-function gz() {
-	echo "orig size    (bytes): "
-	cat "$1" | wc -c
-	echo "gzipped size (bytes): "
-	gzip -c "$1" | wc -c
-}
-
-# whois a domain or a URL
-function whois() {
-	local domain=$(echo "$1" | awk -F/ '{print $3}') # get domain from URL
-	if [ -z $domain ] ; then
-		domain=$1
-	fi
-	echo "Getting whois record for: $domain …"
-
-	# avoid recursion
-	# this is the best whois server
-	# strip extra fluff
-	/usr/bin/whois -h whois.internic.net $domain | sed '/NOTICE:/q'
-}
-
-# function localip(){
-# 	function _localip(){ echo "📶  "$(ipconfig getifaddr "$1"); }
-# 	export -f _localip
-# 	local purple="\x1B\[35m" reset="\x1B\[m"
-# 	networksetup -listallhardwareports | \
-# 		sed -r "s/Hardware Port: (.*)/${purple}\1${reset}/g" | \
-# 		sed -r "s/Device: (en.*)$/_localip \1/e" | \
-# 		sed -r "s/Ethernet Address:/📘 /g" | \
-# 		sed -r "s/(VLAN Configurations)|==*//g"
-# }
-
-
-function pow() {
-  local path=$(pwd)
-  local dir=${path##*/} # http://stackoverflow.com/questions/3162385/how-to-split-a-string-in-shell-and-get-the-last-field
-  cd ~/.pow
-  ln -s $path
-  open "http://$dir.dev"
-}
+# PRT Aliases
+alias ysd="yarn start-dev"
+alias yws="yarn watch:server"
+alias ywc="yarn watch:client"
+alias ybd="yarn build-dev"
+alias cbd="yarn clean && ybd"
+alias ycbd="yarn && cbd"
+alias y="yarn"
+alias ysl="RUNTIME_ENV=local yarn start"
